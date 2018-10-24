4e7ada185a6137dfe37ddbffba5f82e3e9742f0d
jialuzhang <jialuzhang0805@gmail.com>
2018-10-25 01:01:15 +0800

    全部提交

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first'`, () => {
+  it(`should have as title 'untitled1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('first');
+    expect(app.title).toEqual('untitled1');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to first!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to untitled1!');
   });
 });
